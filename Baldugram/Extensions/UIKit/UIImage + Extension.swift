978228f9b1c0094dc47fa9d25a9f66f790ffc28d
Pavel Klopot <pasha555klopot@gmail.com>
2020-04-12 23:59:34 +0300

    Firebase storage for images + some features

---
+++ Baldugram/Extensions/UIKit/UIImage + Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIImage + Extension.swift
+//  Baldugram
+//
+//  Created by Паша Клопот on 4/12/20.
+//  Copyright © 2020 Паша Клопот. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    var scaledToSafeUploadSize: UIImage? {
+      let maxImageSideLength: CGFloat = 480
+      
+      let largerSide: CGFloat = max(size.width, size.height)
+      let ratioScale: CGFloat = largerSide > maxImageSideLength ? largerSide / maxImageSideLength : 1
+      let newImageSize = CGSize(width: size.width / ratioScale, height: size.height / ratioScale)
+      
+      return image(scaledTo: newImageSize)
+    }
+    
+    func image(scaledTo size: CGSize) -> UIImage? {
+      defer {
+        UIGraphicsEndImageContext()
+      }
+      
+      UIGraphicsBeginImageContextWithOptions(size, true, 0)
+      draw(in: CGRect(origin: .zero, size: size))
+
+      return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
